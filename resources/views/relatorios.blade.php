f752d20920abbebf15b1fff45b573a91b1f825a5
Jose Ricardo <josericardojunior@gmail.com>
2025-02-28 20:40:03 -0300

    Alterações na ordem e descrição dos relatórios no frontend

--- resources/views/relatorios.blade.php
+++ resources/views/relatorios.blade.php
@@ -9,20 +9,20 @@
             <div class="card h-100">
                 <div class="card-body">
                     <h5 class="card-title">Relatório 1</h5>
-                    <p class="card-text">Lista livros por edição mais recente</p>
+                    <p class="card-text">Dados de livros por edição mais recente</p>
                     <a href="{{ route('relatorios.livros') }}" class="btn btn-success">
                         Visualizar
                     </a>
                 </div>
             </div>
-        </div>
+        </div>        
 
         <div class="col-md-4">
             <div class="card h-100">
                 <div class="card-body">
                     <h5 class="card-title">Relatório 2</h5>
-                    <p class="card-text">Dados agregados por assunto</p>
-                    <a href="{{ route('relatorios.assuntos') }}" class="btn btn-success">
+                    <p class="card-text">Dados agregados por autor</p>
+                    <a href="{{ route('relatorios.autores') }}" class="btn btn-success">
                         Visualizar
                     </a>
                 </div>
@@ -33,8 +33,8 @@
             <div class="card h-100">
                 <div class="card-body">
                     <h5 class="card-title">Relatório 3</h5>
-                    <p class="card-text">Dados agregados por autor</p>
-                    <a href="{{ route('relatorios.autores') }}" class="btn btn-success">
+                    <p class="card-text">Dados agregados por assunto e editora</p>
+                    <a href="{{ route('relatorios.assuntos') }}" class="btn btn-success">
                         Visualizar
                     </a>
                 </div>
--- resources/views/relatorios/livros.blade.php
+++ resources/views/relatorios/livros.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="d-flex justify-content-between align-items-center mb-4">
-        <h1>Relatório de Livros</h1>
+        <h1>Relatório por Livros</h1>
         <a href="{{ route('relatorios') }}" class="btn btn-secondary">
             <i class="bi bi-arrow-left"></i> Voltar
         </a>
